a6394fec72c5153bce63004d1da89692db840c4e
atorber <atorber@users.noreply.github.com>
2024-09-19 04:40:28 +0000

    deploy: 70b10ec8203247f71f54c49236b01c26241e4611

--- assets/index-CfamxMWD.js
+++ assets/index-CfamxMWD.js
@@ -67,7 +67,7 @@ echo "Start to convert checkpoint..."
 
 AIAK_TRAINING_PATH=/workspace/AIAK-Training-LLM
 CONVERT_CHECKPOINT_PATH="\${AIAK_TRAINING_PATH}/tools/convert_checkpoint"
-MEGATRON_PATH=\${MEGATRON_PATH || "/workspace/AIAK-Megatron"}
+MEGATRON_PATH=\${MEGATRON_PATH:-"/workspace/AIAK-Megatron"}
 
 # 当前不支持 optim 部分转换，通过 --no_save_optim 和 --no_load_optim 关闭；
 python \${CONVERT_CHECKPOINT_PATH}/model.py     --load_platform=huggingface     --save_platform=mcore     --common_config_path=\${CONVERT_CHECKPOINT_PATH}/config/\${MODEL_NAME}.json     --tensor_model_parallel_size=\${TP}     --pipeline_model_parallel_size=\${PP}     --load_ckpt_path=\${LOAD}     --save_ckpt_path=\${SAVE}     --megatron_path=\${MEGATRON_PATH}     --no_save_optim     --no_load_optim     --safetensors
