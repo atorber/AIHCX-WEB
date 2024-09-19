70b10ec8203247f71f54c49236b01c26241e4611
atorber <atorber@163.com>
2024-09-19 12:39:43 +0800

    Update aiak-parms.ts

--- src/components/aiak-parms.ts
+++ src/components/aiak-parms.ts
@@ -37,7 +37,7 @@ echo "Start to convert checkpoint..."
 
 AIAK_TRAINING_PATH=/workspace/AIAK-Training-LLM
 CONVERT_CHECKPOINT_PATH="\${AIAK_TRAINING_PATH}/tools/convert_checkpoint"
-MEGATRON_PATH=\${MEGATRON_PATH || "/workspace/AIAK-Megatron"}
+MEGATRON_PATH=\${MEGATRON_PATH:-"/workspace/AIAK-Megatron"}
 
 # 当前不支持 optim 部分转换，通过 --no_save_optim 和 --no_load_optim 关闭；
 python \${CONVERT_CHECKPOINT_PATH}/model.py \
