98f58aae2b30b7bf137593f50b35c49261cda9e4
luyuchao <luyuchao@baidu.com>
2025-08-14 18:30:42 +0800

    删除多个环境配置文件、ESLint、Prettier、Stylelint配置文件及其他相关文件，清理项目结构以简化维护。

--- web-pro/src/components/ComingSoon/index.ts
+++
@@ -1,3 +0,0 @@
-import ComingSoon from './src/ComingSoon.vue'
-
-export { ComingSoon }
--- web-pro/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import type { App } from 'vue/dist/vue.esm-bundler'
-import { Icon } from './Icon'
-import { BaseButton } from './Button'
-
-export const setupGlobCom = (app: App<Element>): void => {
-  app.component('Icon', Icon)
-  app.component('BaseButton', BaseButton)
-}
